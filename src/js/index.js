edf3aaa5c67799d404608ff387d1c31a1c4b1865
Xingan Wang <wangxgwxg@gmail.com>
2017-05-19 11:13:46 -0700

    background & snake entry

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,11 @@
-var PIXI = require('pixi.js')
+const PIXI = require('pixi.js')
 
-var renderer = new PIXI.CanvasRenderer(window.innerWidth, window.innerHeight)
+// config
+const worldSize = {w: 800, h: 600}
+    , gridSize  = {w: 20, h: 20}
+    , backgroundColor = 0x779966
 
-document.body.appendChild(renderer.view)
+const app = new PIXI.Application(800, 600, {backgroundColor: backgroundColor})
+document.body.appendChild(app.view)
 
-var stage = new PIXI.Stage
 
-var zombieTexture = PIXI.Texture.fromImage('../assets/zombie.png')
-var zombie = new PIXI.Sprite(zombieTexture)
-
-zombie.position.x = window.innerWidth / 2 - 150
-zombie.position.y = window.innerHeight / 2 - 150
-
-stage.addChild(zombie)
-
-function draw() {
-  renderer.render(stage)
-  requestAnimationFrame(draw)
-}
-
-draw()
