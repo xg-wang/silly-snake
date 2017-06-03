0f4b674788db30f6471585b8c303dc61f71ab4b2
Xingan Wang <wangxgwxg@gmail.com>
2017-06-03 10:54:07 -0700

    make apple larger

--- src/js/snake/apple.js
+++ src/js/snake/apple.js
@@ -9,7 +9,9 @@ class Apple {
     const texture = PIXI.loader.resources[
       '../assets/snakeset.png'
     ].texture
-    texture.frame = new PIXI.Rectangle(192, 128, 64, 64);
+    // texture.frame = new PIXI.Rectangle(256-64, 192-64, 64, 64);
+    const w = 14
+    texture.frame = new PIXI.Rectangle(256-64+w, 192-64+w, 64-w*2, 64-w*2);
     this.sprite = new PIXI.Sprite(texture)
     this.sprite.width = gridSize.w
     this.sprite.height = gridSize.h
