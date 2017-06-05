e1cf52f5c4521ffd21c5db3ee7154ec583c78715
ttylcc <ttylacm@126.com>
2017-06-04 21:17:51 -0700

    there is bug!!!oldQValue

--- src/js/learning/index.js
+++ src/js/learning/index.js
@@ -34,57 +34,57 @@ class QLearner {
   }
 
   updateQ(state) {
-    // let rwd = this.reward()
-    // this.oldQvalue += this.learnRate * (rwd + this.gamma * this.currQValue - this.oldQValue)
+    let rwd = this.reward()
+    this.oldQValue += this.learnRate * (rwd + this.gamma * this.currQValue - this.oldQValue)
   }
 
   reward() {
-    // if (this.snake.head.position.x== this.apple.position.x && this.snake.head.position.y == this.apple.position.y) {
-    //   // apple become a part of body, need to rewrite grow()
-    //   this.snake.grow(this.snake.head)
+    if (this.snake.head.position.x== this.apple.position.x && this.snake.head.position.y == this.apple.position.y) {
+      // apple become a part of body, need to rewrite grow()
+      // this.snake.grow(this.snake.head)
+      // check apple position, apple cannot be on snake body
+      // this.apple.moveTo(this.nextApplePosition)
+      this.explorationRate = this.explorationRate / 3
+      this.score = this.score + 1
+      this.restartCount = 0
+      return 1000.0
+    }
+    else if (!this.snake.abstractMap.snakeMove(this.snake.head.position, this.snake.tail.position)) {
+      this.score = 0
+      this.restartCount = 0
+      this.snake.reset()
+      this.restartCount = this.restartCount + 1
+      return -100000.0
+    }
+    else if (this.snake._checkBoundary(this.snake.head.position)) {
+      this.snake.reset()
+      this.restartCount = this.restartCount + 1
+      return -1000.0
+    }
 
-    //   // check apple position, apple cannot be on snake body
-    //   this.apple.moveTo(this.nextApplePosition)
-    //   this.explorationRate = this.explorationRate / 3
-    //   this.score = this.score + 1
-    //   this.restartCount = 0
-    //   return 1000.0
-    // }
-    // else if (checkEncounterBody()) {
-    //   this.score = 0
-    //   this.restartCount = 0
-    //   // restart()
-    //   this.restartCount = this.restartCount + 1
-    //   return -100000.0
-    // }
-    // else if (checkEncounterWall()) {
-    //   // restart
-    //   this.restartCount = this.restartCount + 1
-    //   return -1000.0
-    // }
+    if (this.restartCount > 50) {
+      this.explorationRate = 20
+    }
 
-    // if (this.restartCount > 50) {
-    //   this.explorationRate = 20
-    // }
+    rwd = Math.sqrt((this.snake.head.position.x - this.apple.position.x) * (this.snake.head.position.x -
+    this.apple.position.x) + (this.snake.head.position.y - this.apple.position.y) * (this.snake.head.position.y -
+    this.apple.position.y))
 
-    // rwd = sqrt((this.snake.head.position.x - this.apple.position.x) * (this.snake.head.position.x -
-    //   this.apple.position.x) + (this.snake.head.position.y - this.apple.position.y) * (this.snake.head.position.y -
-    //   this.apple.position.y))
-
-    // return -rwd
+    return -rwd
   }
 
   getQValue(dir) {
-    const headPos = this.snake.head.position.clone()
+    // const headPos = this.snake.head.position.clone()
+    const headPos = { position: {x: this.snake.head.position.x, y: this.snake.head.position.y} }
     this.snake.toNextDirection(headPos, dir)
     return Math.sqrt(
-      Math.pow((headPos.x - this.apple.position.x), 2) +
-      Math.pow((headPos.y - this.apple.position.y), 2)
+      Math.pow((headPos.position.x - this.apple.position.x), 2) +
+      Math.pow((headPos.position.y - this.apple.position.y), 2)
     )
   }
 
   directionFromMaxQValue(dirs) {
-    const qValues = dirs.map(d => getQValue(d))
+    const qValues = dirs.map(d => this.getQValue(d))
     this.currQValue = _.max(qValues)
     return DIRECTIONS[qValues.indexOf(this.currQValue)]
   }
